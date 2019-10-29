6a84f316fdf04c4954b34c3899ff6d2ee383d03d
Student <example@example.com>
2019-10-29 14:06:30 -0700

    practice making API calls in MVC

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCApiCall.Models;
+
+namespace MVCApiCall.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var allArticles = Article.GetArticles(EnvironmentVariables.ApiKey);
+            return View(allArticles);
+        }
+    }
+}
---
+++ Models/EnvironmentVariables.cs
@@ -0,0 +1,7 @@
+namespace MVCApiCall.Models
+{
+    public static class EnvironmentVariables
+    {
+        public static string ApiKey = "Uj39yfYPr8uQNnANZZ9deJauLDjuMP6U";
+    }
+}
